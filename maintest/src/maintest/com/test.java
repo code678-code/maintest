8ea3be85a992c74d7650beeb762bd6e22bb5c375
code678-code <QKcode@163.com>
2020-05-20 18:15:07 +0800

    更新一个方法

--- maintest/src/maintest/com/test.java
+++ maintest/src/maintest/com/test.java
@@ -3,5 +3,11 @@ package maintest.com;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("远程下载测试");
+		test();
+	}
+
+	private static void test() {
+		System.out.println("这是一个方法");
+		
 	}
 }
